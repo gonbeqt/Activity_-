9a9ff520ac3ff2565b97b4bf06a33d73a0856b74
gonbeqt <143175525+gonbeqt@users.noreply.github.com>
2024-12-11 09:35:30 +0800

    sekund comit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.myapplication
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapteroflist: ArrayList<CountryInfo>
+    private lateinit var btnExit: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +45,14 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize the adapter's data
         adapteroflist = ArrayList(countriesWithFlags)
-
+        btnExit = findViewById(R.id.btnExit)
         // Access the ListView directly using binding
         val listView: ListView = binding.listView  // Access ListView via binding
         val adapter = listadapter(this, adapteroflist)  // Set custom adapter
         listView.adapter = adapter  // Set the adapter to the ListView
+
+        btnExit.setOnClickListener {
+            finish()
+        }
     }
 }
